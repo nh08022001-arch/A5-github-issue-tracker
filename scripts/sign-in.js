b893279febd7a3f2e9eca12df399a7512157a095
nh08022001-arch <nh08022001@gmail.com>
2026-03-07 21:10:20 +0100

    signin form coded

---
+++ scripts/sign-in.js
@@ -0,0 +1,18 @@
+
+document.getElementById("signin-btn").addEventListener("click", function(){
+    //get user name
+    const textInput = document.getElementById("input-text");
+    const userName = textInput.value;
+    //get password
+    const inputPassword = document.getElementById("input-password");
+    const password = inputPassword.value;
+    //match password and username
+    if(userName == "admin" && password == "admin123"){
+        //alert("login success");
+        window.location.assign("/main-project.html");
+
+    }else{
+        //alert("login failled");
+        return;
+    }
+})
